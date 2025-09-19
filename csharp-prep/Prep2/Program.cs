1b86ee96d245aafe908c308fc811db5c291f5415
Dustin <dustinnewby372@gmail.com>
2025-09-18 21:30:57 -0600

    Got the basic code in the program!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,6 +8,26 @@ class Program
         string grade = Console.ReadLine();
 
         int gradePercentage = int.Parse(grade);
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("You got an A! nice!");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("You got a B! Well done, but try a little harder next time.");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You got a C. Hey, at least you passed!");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("You got a D. I think this counts as failing...");
+        }
+        else
+        {
+            Console.WriteLine("You got an F. Maybe try the class before again, then take this one again.");
+        }
 
     }
 }
