1cd42e4f37fd229dfbd6760127c693c1e321e513
Dustin <dustinnewby372@gmail.com>
2025-09-19 11:07:34 -0600

    Got a basic frame work going.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        /*
+
+        What will be the the random number!
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        */
+        // this is going to be our test number
+        int magicNumber = 36;
+
+
+        Console.WriteLine("Welcome to guess the magic number!");
+        Console.Write("Let's get started. what is your guess for the magic number? ");
+        string firstGuess = Console.ReadLine();
+        int guess = int.Parse(firstGuess);
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("You guessed under the number");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("You guessed over the number");
+        }
+        else
+        {
+            Console.WriteLine("That was the magic number! Congrats!");
+        }
+
     }
 }
