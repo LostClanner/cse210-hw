b3b3c96a96f2d8f9808fa9f4a2ea1ce0af3e0436
Dustin <dustinnewby372@gmail.com>
2025-09-23 11:14:37 -0600

    Pre tester

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This is the list of Jobs and positions held
         JobHistory job1 = new JobHistory();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
@@ -17,6 +18,17 @@ class Program
         job2._endYear = 2023;
 
 
+        // This calls the resume and has the name and lists the jobs afterward
+        Resume myResume = new Resume();
+        // this is how you set the name
+        myResume._name = "Dustin Newbold";
+
+        //This gives resume the jobs
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // This displays what has been made
+        myResume.Display();
         
     }
 
