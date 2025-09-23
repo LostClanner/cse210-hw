3fff90f12b9450d664a43d60d7b9fb2d96861e6c
Dustin <dustinnewby372@gmail.com>
2025-09-23 10:30:48 -0600

    Got Job history

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,4 +6,16 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
     }
+
+
+
+
+
+    public class JobHistory
+    {
+        public string _jobTitle;
+        public string _company;
+        public int _startYear;
+        public int _endYear;
+    }
 }
