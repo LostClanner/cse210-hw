a85dcd7512c3ce374f2f400decf96d8fdb8d362c
Dustin <dustinnewby372@gmail.com>
2025-09-24 19:15:33 -0600

    Getting some items in here

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Just a little welcome message
-        Console.WriteLine("Welcome to the journal program!");
+        Console.WriteLine("------------Journal home page------------");
 
 
         //Getiing the user input
@@ -15,6 +15,38 @@ class Program
         Console.WriteLine("Enter 3 to load your journal file");
         Console.WriteLine("Enter 4 to save your journal");
         Console.WriteLine("Enter 5 to quit");
+        int Selection = int.Parse(Console.ReadLine());
+
+
+        //Selection logic and where to go now
+        if (Selection == 1)
+        {
+
+        }
+        else if (Selection == 2)
+        {
+
+        }
+        else if (Selection == 3)
+        {
+
+        }
+        else if (Selection == 4)
+        {
+
+        }
+        else if (Selection == 5)
+        {
+
+        }
+        else
+        {
+
+        }
+        
+
+        
+        
 
     }
     
