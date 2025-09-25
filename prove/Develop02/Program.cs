a2d0b55f3e3cd07dd872c8be20820bd4db761eba
Dustin <dustinnewby372@gmail.com>
2025-09-24 19:03:23 -0600

    Load the begining messages

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Just a little welcome message
+        Console.WriteLine("Welcome to the journal program!");
+
+
+        //Getiing the user input
+        Console.WriteLine("Type a number between 1 and 5 to select an option:");
+        Console.WriteLine("Enter 1 to view your journal");
+        Console.WriteLine("Enter 2 to add a new entry");
+        Console.WriteLine("Enter 3 to load your journal file");
+        Console.WriteLine("Enter 4 to save your journal");
+        Console.WriteLine("Enter 5 to quit");
+
     }
+    
 }
