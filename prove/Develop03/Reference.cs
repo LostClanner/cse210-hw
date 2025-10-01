6589256d9f645094fc457ce5ccaa5469b3c8019f
Dustin Newbold <dustinnewby372@gmail.com>
2025-09-30 22:14:08 -0600

    Getting started with Reference

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Reference
+{
+    //private things
+    //sting book
+    // chapter int
+    // versestart int
+    //verseend int
+
+    //public things
+    //reference book chapter verse
+    //reference book chapter versestart verse end
+    //to string string
+}
