47e83fafd09e189810fede8335e1b05ca5f747fb
Dustin Newbold <dustinnewby372@gmail.com>
2025-09-30 22:49:01 -0600

    A little fram.work

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,15 +1,31 @@
 using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
 
 public class Reference
 {
-    //private things
-    //sting book
-    // chapter int
-    // versestart int
-    //verseend int
+    //--private things--
+    private string Book = null;
+    private int Chapter = 0;
+    private int Verse = 0;
+    private int VerseStart = 0;
+    private int VerseEnd = 0;
 
-    //public things
+
+    //--public things--
     //reference book chapter verse
+    Reference(Book, Chapter, Verse)
+    {
+
+
+
+    }
+    Reference(Book, Chaper, VerseStart, VerseEnd)
+    {
+
+
+
+    }
     //reference book chapter versestart verse end
     //to string string
 }
