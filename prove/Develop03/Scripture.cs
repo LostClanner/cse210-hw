3146a66325edf03500fb9207eb1e954c271edf95
Dustin <dustinnewby372@gmail.com>
2025-10-04 17:33:01 -0600

    trying to fix the Scriptures.cs

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,12 +2,57 @@ using System;
 
 public class Scripture()
 {
-    private string _reference;
-    private List<TheText> _theText;
+    private Reference _reference;
+    private new List<_theText>;
 
-    public HiddenMaker(Reference, TheText)
+    public HiddenMaker(Reference reference, string TheText)
+    {
+        reference = _reference;
+        TheText = new List<_theText>();
+
+        foreach (string TheText in text.split(""))
+        {
+            _theText.Add(new Word(TheText));
+        }
+
+    }
+
+    public string GetDisplayText()
+    {
+        string ReferenceText = _reference.GetDisplayText;
+        string TheActualStricpture = "";
+
+        foreach (Word TheText in _theText)
+        {
+            TheActualStricpture += TheText.GetDisplayText() + " ";
+        }
+        return ReferenceText + " " + TheActualStricpture.Trim();
+
+    }
 
 
+    public void HideRandomWords(int TheOneThatHides)
+    {
+    Random rand = new Random();
+    for (int i = 0; i < TheOneThatHides; i++)
+    {
+        int index = rand.Next(_theText.Count);
+        _theText[index].Hide();
+    }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _theText)
+        {
+            if (!Word._isHidden())  // if even one word is visible
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 
 }
