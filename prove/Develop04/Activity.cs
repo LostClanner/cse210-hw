b5ba3df41d51943f4150a444606896839d9139fd
Dustin <dustinnewby372@gmail.com>
2025-10-08 18:12:18 -0600

    added more

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -9,5 +9,30 @@ public class Activity
     private string _description = "";
     private int _duration = 0;
 
+    public Activity(string activityName, string description, int duration)
+    {
+        _activityName = activityName;
+        _description = description;
+        _duration = duration;
+    }
+
+    public void StartActivity()
+    {
+
+
+
+    }
+
+    public void EndActivity()
+    {
+            
+    }
+
+
+    public void Run()
+    {
+        
+    }
+    
 
 }
