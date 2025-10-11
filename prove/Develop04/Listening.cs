d94a4f76bf73cbc06844b9711e1e62072989c9e7
Dustin <dustinnewby372@gmail.com>
2025-10-11 00:09:48 -0600

    random questions

--- prove/Develop04/Listening.cs
+++ prove/Develop04/Listening.cs
@@ -14,7 +14,7 @@ public class ReflectionActivity : Activity
         "Think of a time when you did something truly selfless."
     };
 
-    private List<string> _reflect = new List<string>
+    private List<string> _question = new List<string>
     {
         "Why was this experience meaningful to you?",
         "Have you ever done anything like this before?",
@@ -50,6 +50,14 @@ public class ReflectionActivity : Activity
     {
         int index = _random.Next(_prompts.Count);
         string randomPrompt = _prompts[index];
+        return randomPrompt;
+
+    }
+
+    private string GetRandomQuestion()
+    {
+        int index = _random.Next(_question.Count);
+        string randomPrompt = _question[index];
         return randomPrompt;
 
     }
