70f4c23b29af4c809579fb87bd6f0a57e1d5a66d
Dustin <dustinnewby372@gmail.com>
2025-10-11 00:19:40 -0600

    Corrected the filename and think I found a way for the activity

--- prove/Develop04/Reflection.cs
+++ prove/Develop04/Reflection.cs
@@ -37,11 +37,16 @@ public class ReflectionActivity : Activity
     public ReflectionActivity(int duration)
         : base("reflection activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.", duration)
     {
-
     }
     public override void Run()
     {
         StartActivity();
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while(DateTime.Now < endTime)
+        {
+            
+        }
 
         EndActivity();
     }
@@ -62,11 +67,6 @@ public class ReflectionActivity : Activity
 
     }
     
-
-
-
-
-
 
 
 
