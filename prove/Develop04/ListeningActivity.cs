3b8a3a223c390201bf800c3ded6097df514e841b
Dustin <dustinnewby372@gmail.com>
2025-10-11 10:42:21 -0600

    Getting this done

---
+++ prove/Develop04/ListeningActivity.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+public class ListingActivity : Activity
+{
+
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+
+    private static Random _random = new Random();
+
+
+
+
+
+    public ListingActivity(int duration)
+        : base("listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.", duration)
+    {
+    }
+    public override void Run()
+    {
+        StartActivity();
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        GetRandomPrompt();
+        ShowSpinner(7);
+        Console.WriteLine("\nStart listing items now:");
+
+        while(DateTime.Now < endTime)
+        {
+            GetRandomPrompt();
+            ShowCountdown(10);
+            
+        }
+
+        EndActivity();
+    }
+
+    private void GetRandomPrompt()
+    {
+        int index = _random.Next(_prompts.Count);
+        string randomPrompt = _prompts[index];
+        Console.WriteLine(randomPrompt);
+
+    }
+
+
+
+
+
+}
--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -9,7 +9,7 @@ public class ReflectionActivity : Activity
     private List<string> _prompts = new List<string>
     {
         "Think of a time when you stood up for someone else.",
-        "Think of a time when you did something really difficult.x",
+        "Think of a time when you did something really difficult.",
         "Think of a time when you helped someone in need.",
         "Think of a time when you did something truly selfless."
     };
