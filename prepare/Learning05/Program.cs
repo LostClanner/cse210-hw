44ee1ba466ddc1ca126a4bc0660ae9d725eb48c1
Dustin <dustinnewby372@gmail.com>
2025-10-14 15:14:10 -0600

    This is correct

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -21,7 +21,7 @@ class Program
             string color = s.GetColor();
             double area = s.GetArea();
 
-            Console.WriteLine($"The area of your shape is {area:2f} and the color is {color}.");
+                Console.WriteLine($"The area of your shape is {area:f2} and the color is {color}.");
         }
     }
 }
