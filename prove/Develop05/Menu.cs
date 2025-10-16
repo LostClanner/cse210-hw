701bc4377d3befa9e1715728e7660c0aba7b7e52
Dustin <dustinnewby372@gmail.com>
2025-10-16 10:41:49 -0600

    A little test

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -1,1 +1,39 @@
 using System;
+
+
+public class Menu
+{
+    public void Run()
+    {
+
+
+    }
+
+    private void ListGoals()
+    {
+
+
+    }
+
+    private void MakeGoals()
+    {
+
+
+    }
+
+    private void CompleteGoals()
+    {
+
+    }
+
+    private void SaveGoals()
+    {
+
+    }
+    
+    private void LoadGoals()
+    {
+        
+    }
+
+}
