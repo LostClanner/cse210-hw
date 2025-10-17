d7779eed735ea82cdb4ec3b0d77a2c90c7016464
Dustin <dustinnewby372@gmail.com>
2025-10-17 13:04:59 -0600

    It wont get the menu things, I need to figure that out

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -12,8 +12,27 @@ class Program
         {
             Console.WriteLine("Welcome to Goal Setter Quest");
             Console.WriteLine("Please select what you would like to do");
+            Console.WriteLine("1. Make a new goal");
+            Console.WriteLine("2. List all of your goals");
+            Console.WriteLine("0. Quit ");
+            Console.Write("> ");
 
-        
+            switch (choice)
+            {
+                case 1:
+                    MakeGoals();
+                    break;
+
+                case 2:
+
+                case 0:
+
+                default:
+                    Console.WriteLine("Incorrect answer, please type a real number");
+                    Thread.Sleep(3000);
+                    break;
+            }
+
 
         }
 
