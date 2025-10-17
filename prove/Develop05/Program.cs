fefe8b1078a4fd129fa465449b6bf1ec86db235f
Dustin <dustinnewby372@gmail.com>
2025-10-17 12:58:16 -0600

    Started the program, but might move it to the menu class

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        bool isRunning = true;
+        int choice = -1;
+
+        while (isRunning)
+        {
+            Console.WriteLine("Welcome to Goal Setter Quest");
+            Console.WriteLine("Please select what you would like to do");
+
+        
+
+        }
+
     }
 }
