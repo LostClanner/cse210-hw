ebff4293da0921038b8fdd4db699971a7d479e6d
Dustin <dustinnewby372@gmail.com>
2025-10-20 16:04:10 -0600

    This is a first test of the most basic logic

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Deck d1 = new Deck();
+        d1.MakeNewDeck();
+
+        
     }
 }
