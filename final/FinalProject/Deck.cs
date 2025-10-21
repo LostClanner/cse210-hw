a9fc7636b376b8dc8403b15f6e7f91675fdfa8ba
Dustin <dustinnewby372@gmail.com>
2025-10-21 10:17:09 -0600

    Started the game logic

--- final/FinalProject/Deck.cs
+++ final/FinalProject/Deck.cs
@@ -30,7 +30,7 @@ public class Deck
             i++;
         }
 
-        // ShuffleDeck();
+        ShuffleDeck();
 
 
 
---
+++ final/FinalProject/Game.cs
@@ -0,0 +1,44 @@
+using System;
+
+
+public class Game
+{
+    private _deck
+    private _players
+    private _discardPile
+    private _currentPlayer
+    private _isReversed
+
+
+
+    public Game()
+    {
+
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void NextTurn()
+    {
+
+    }
+
+    public void PlayerPlayCard()
+    {
+
+    }
+
+    public void PlayerDrawCard()
+    {
+
+    }
+
+    public void HandelCardEffect()
+    {
+        
+    }
+
+}
