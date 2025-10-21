e78d2c26317f53df7f2987e91f1e1ff624001b9d
Dustin <dustinnewby372@gmail.com>
2025-10-21 10:52:07 -0600

    Basic player function

---
+++ final/FinalProject/Player.cs
@@ -0,0 +1,32 @@
+using System;
+
+
+
+public class Player
+{
+    private List<Card> _playerHand = new List<Card>();
+
+    public Player()
+    {
+        
+    }
+
+    public void PlayCard()
+    {
+
+    }
+
+    public void AddCardToHand()
+    {
+        
+    }
+
+    public void GetHand()
+    {
+        foreach (Card card in _playerHand)
+        {
+            Console.Write($"{_playerHand} ");
+        }
+    }
+    
+}
