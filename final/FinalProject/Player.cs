f61be08ae6060931ddc0b7257e2e6798d4986cd6
Dustin <dustinnewby372@gmail.com>
2025-10-21 10:56:57 -0600

    oops

--- final/FinalProject/Player.cs
+++ final/FinalProject/Player.cs
@@ -25,7 +25,7 @@ public class Player
     {
         foreach (Card card in _playerHand)
         {
-            Console.Write($"{_playerHand} ");
+            Console.Write($"{card} ");
         }
     }
     
