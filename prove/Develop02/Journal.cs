b20f47277981df2bb46c117bd9ba84fa12824fb0
Dustin <dustinnewby372@gmail.com>
2025-10-28 11:56:43 -0600

    got the framework for the journal

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -6,13 +6,34 @@ using System.Text.Json;
 
 public class Journal
 {
-    private List<> ;
+    private List<Entry> _entries;
 
     public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
+
+    public void AddNewEntry()
+    {
+
+    }
+
+    public void DisplayEntry()
+    {
+
+    }
+
+    public void SaveEntry()
+    {
+
+    }
+    
+    public void LoadEntry()
     {
         
     }
 
-
+    // add display save load 
 
 }
