07ba23753001ae2dd82ba67334deb0b26a1c263b
Dustin <dustinnewby372@gmail.com>
2025-10-28 13:13:43 -0600

    Working on the main switch

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,6 +7,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Journal j1 = new Journal();
+        PromptGen pG1 = new PromptGen();
+        bool contineRun = true;
+
+        Console.WriteLine("--------Welcome to the journal--------");
+
+        while (contineRun)
+        {
+            Console.WriteLine("Pick a number between 1-5 to select an option");
+            Console.WriteLine("1. Write a new journal entry");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. load");
+            Console.WriteLine("4. save");
+            Console.WriteLine("3. quit");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
