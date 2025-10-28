59e166a6d0c0c599ecf57d6124e78b1bd099b19d
Dustin <dustinnewby372@gmail.com>
2025-10-28 11:42:41 -0600

    Started to work on journal, but it will use entries a lot, So I will move to work on that

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -2,3 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+
+
+public class Journal
+{
+    private List<> ;
+
+    public Journal()
+    {
+        
+    }
+
+
+
+}
