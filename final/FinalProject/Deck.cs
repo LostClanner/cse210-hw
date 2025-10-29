8a5d547e59d88fe0165b10a86ea889b6c1425339
Dustin <dustinnewby372@gmail.com>
2025-10-28 18:36:00 -0600

    The finished project

--- final/FinalProject/Deck.cs
+++ final/FinalProject/Deck.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Drawing;
-using System.Reflection.Metadata.Ecma335;
 using System.Linq;
 
 
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,30 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Game g1 = new Game();
-        Console.WriteLine("Welcome to Uno!");
-        Console.WriteLine("----------------");
-        Console.WriteLine("Press 1 to start a new game. Press 0 to exit.");
-        string choice = Console.ReadLine();
-        
-        switch (choice)
+        bool run = true;
+        while (run)
         {
-            case "1":
-                Console.WriteLine("How many players would you like to play with?");
-                if (int.TryParse(Console.ReadLine(), out int numPlayers))
+            Console.Clear();
+            Console.WriteLine("Welcome to Uno!");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Press 1 to start a new game. Press 0 to exit.");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Game g1 = new Game(); 
+
+                    Console.WriteLine("How many players would you like to play with?");
+                    if (int.TryParse(Console.ReadLine(), out int numPlayers))
                     {
-                        g1.Run(numPlayers);
+                        if (numPlayers > 1 && numPlayers < 10)
+                        {
+                            g1.Run(numPlayers);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid number of players. (Must be 2-9)");
+                            Console.WriteLine("Press enter to return to menu.");
+                            Console.ReadLine();
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Invalid number of players.");
+                        Console.WriteLine("Invalid input.");
+                        Console.WriteLine("Press enter to return to menu.");
+                        Console.ReadLine();
                     }
-                break;
-            case "0":
-                return;
-            default:
-                Console.WriteLine("Invalid choice. Exiting.");
-                break;
+                    break;
+                case "0":
+                    run = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    Console.WriteLine("Press enter to return to menu.");
+                    Console.ReadLine();
+                    break;
+            }
         }
 
     }
